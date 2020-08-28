e103fb82323d29edc53677e5889d61d8744f968a
ZackStrater <68521156+ZackStrater@users.noreply.github.com>
2020-08-28 18:38:02 -0400

    adding fragments file

---
+++ build_fragments.py
@@ -0,0 +1,4 @@
+import fragments
+import random
+
+#def build_molecule():
---
+++ fragments.py
@@ -0,0 +1,13 @@
+fragments = {}
+
+fragments["indole"] = "C12=CC=CC=C1NC=C2"
+fragments["benzimidazole"] = "C12=CC=CC=C1NC=N2"
+fragments["imidazole"] = "C1=CN=CN1"
+fragments["benzene"] = "C1=CC=CC=C1"
+fragments["naphthalene"] = "C12=CC=CC=C1C=CC=C2"
+fragments["cyclohexyl"] = "C1CCCCC1"
+fragments["methyl"] = "C"
+fragments["ethyl"] = "CC"
+fragments["butyl"] = "CCCC"
+fragments["pyrrolidine"] = "C1CCNC1"
+fragments["cyclopropyl"] = "C1CC1"
