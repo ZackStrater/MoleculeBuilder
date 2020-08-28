136c13765c19f8def65007302643ea3d32b31775
ZackStrater <68521156+ZackStrater@users.noreply.github.com>
2020-08-28 11:42:17 -0400

    trying pycharm commit

--- molecule_builder.py
+++ molecule_builder.py
@@ -339,8 +339,3 @@ convert_to_smiles(molecule2, benzene4.c4)
         # then either mark adjacent atom, or mark atom once number of times fragment has been visited == num of atoms
 
 
-# blahdslfkjhalfhaksd
-# akljflkajdsfljkh
-#lasdljfha;sdjf
-#ljsfh;ljajsd;fkl
-#akljdjhlakjdsfh
